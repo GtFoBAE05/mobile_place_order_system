91b6b9309acafb010256a7d32359d1ce62cacc35
GtFoBAE05 <y.imannuel@gmail.com>
2025-01-05 04:14:26 +0700

    feat: Create product and product type entity

---
+++ src/main/java/com/imannuel/mobile_place_order_system/entity/ProductType.java
@@ -0,0 +1,20 @@
+package com.imannuel.mobile_place_order_system.entity;
+
+import com.imannuel.mobile_place_order_system.constant.DatabaseConstants;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = DatabaseConstants.PRODUCT_TYPE_TABLE)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+}
