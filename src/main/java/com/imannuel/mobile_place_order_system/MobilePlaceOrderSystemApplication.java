f33ac333f05a629a5c282b70d09197683052b906
GtFoBAE05 <y.imannuel@gmail.com>
2025-01-04 15:23:04 +0700

    Initial Commit

---
+++ src/main/java/com/imannuel/mobile_place_order_system/MobilePlaceOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.imannuel.mobile_place_order_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilePlaceOrderSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobilePlaceOrderSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imannuel/mobile_place_order_system/MobilePlaceOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imannuel.mobile_place_order_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilePlaceOrderSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
