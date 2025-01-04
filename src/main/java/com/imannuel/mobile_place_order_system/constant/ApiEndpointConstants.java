d82eb86af407c7975045ac36074ec1f13f2dd5ef
GtFoBAE05 <y.imannuel@gmail.com>
2025-01-05 04:16:00 +0700

    feat: Create constants product and product type

---
+++ src/main/java/com/imannuel/mobile_place_order_system/constant/ApiEndpointConstants.java
@@ -0,0 +1,6 @@
+package com.imannuel.mobile_place_order_system.constant;
+
+public class ApiEndpointConstants {
+    public static final String PRODUCT_ENDPOINT = "/api/products";
+    public static final String PRODUCT_TYPE_ENDPOINT = "/api/product-types";
+}
---
+++ src/main/java/com/imannuel/mobile_place_order_system/constant/DatabaseConstants.java
@@ -0,0 +1,6 @@
+package com.imannuel.mobile_place_order_system.constant;
+
+public class DatabaseConstants {
+    public static final String PRODUCT_TYPE_TABLE = "m_product_type";
+    public static final String PRODUCT_TABLE = "m_products";
+}
