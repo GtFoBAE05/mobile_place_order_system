4342d3ae9cc8eb759c2f64d3a4ba87ca50f5440e
GtFoBAE05 <y.imannuel@gmail.com>
2025-01-05 04:50:46 +0700

    feat: Create customer entity

---
+++ src/main/java/com/imannuel/mobile_place_order_system/entity/Customer.java
@@ -0,0 +1,22 @@
+package com.imannuel.mobile_place_order_system.entity;
+
+import com.imannuel.mobile_place_order_system.constant.DatabaseConstants;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = DatabaseConstants.CUSTOMERS_TABLE)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private String address;
+}
