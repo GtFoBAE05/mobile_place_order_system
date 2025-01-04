3d36b73921baf1211a48595309ffd9ba844a404d
GtFoBAE05 <y.imannuel@gmail.com>
2025-01-05 04:50:38 +0700

    feat: Create customer constant

--- src/main/java/com/imannuel/mobile_place_order_system/constant/ApiEndpointConstants.java
+++ src/main/java/com/imannuel/mobile_place_order_system/constant/ApiEndpointConstants.java
@@ -1,6 +1,7 @@
 package com.imannuel.mobile_place_order_system.constant;
 
 public class ApiEndpointConstants {
+    public static final String CUSTOMER_ENDPOINT = "/api/customers";
     public static final String PRODUCT_ENDPOINT = "/api/products";
     public static final String PRODUCT_TYPE_ENDPOINT = "/api/product-types";
 }
--- src/main/java/com/imannuel/mobile_place_order_system/constant/DatabaseConstants.java
+++ src/main/java/com/imannuel/mobile_place_order_system/constant/DatabaseConstants.java
@@ -1,6 +1,7 @@
 package com.imannuel.mobile_place_order_system.constant;
 
 public class DatabaseConstants {
+    public static final String CUSTOMERS_TABLE = "m_customers";
     public static final String PRODUCT_TYPE_TABLE = "m_product_type";
     public static final String PRODUCT_TABLE = "m_products";
 }
