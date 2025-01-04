6a3ebe25634970e3b78b31b4db4a30a37ecc6265
GtFoBAE05 <y.imannuel@gmail.com>
2025-01-05 04:14:38 +0700

    feat: Create product and product type repository

---
+++ src/main/java/com/imannuel/mobile_place_order_system/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.imannuel.mobile_place_order_system.repository;
+
+import com.imannuel.mobile_place_order_system.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, String>, JpaSpecificationExecutor<Product> {
+}
