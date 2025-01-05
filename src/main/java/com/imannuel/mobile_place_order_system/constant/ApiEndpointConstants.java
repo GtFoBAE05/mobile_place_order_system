2790fe00621629eed60ac0bcbd3851a8b5fc47ad
GtFoBAE05 <y.imannuel@gmail.com>
2025-01-05 17:05:26 +0700

    feat: Create cart constant

--- src/main/java/com/imannuel/mobile_place_order_system/constant/ApiEndpointConstants.java
+++ src/main/java/com/imannuel/mobile_place_order_system/constant/ApiEndpointConstants.java
@@ -4,4 +4,5 @@ public class ApiEndpointConstants {
     public static final String CUSTOMER_ENDPOINT = "/api/customers";
     public static final String PRODUCT_ENDPOINT = "/api/products";
     public static final String PRODUCT_TYPE_ENDPOINT = "/api/product-types";
+    public static final String CART_ENDPOINT = "/api/carts";
 }
--- src/main/java/com/imannuel/mobile_place_order_system/constant/MessageConstants.java
+++ src/main/java/com/imannuel/mobile_place_order_system/constant/MessageConstants.java
@@ -8,7 +8,7 @@ public class MessageConstants {
     public static final String INVALID_SORT_DIRECTION = "Invalid sort direction";
 
     //customer
-    public static final String CUSTOMER_NOT_FOUND = "Customer Not Found";
+    public static final String CUSTOMER_NOT_FOUND = "Customer not found";
     public static final String CUSTOMER_SUCCESS_CREATE = "Success create product type";
     public static final String CUSTOMER_SUCCESS_FIND = "Success find customer";
     public static final String CUSTOMER_SUCCESS_FETCH_ALL = "Success fetch all customers";
@@ -16,7 +16,7 @@ public class MessageConstants {
     public static final String CUSTOMER_SUCCESS_DELETE = "Success delete customer";
 
     //product type
-    public static final String PRODUCT_TYPE_NOT_FOUND = "Product type Not Found";
+    public static final String PRODUCT_TYPE_NOT_FOUND = "Product type not found";
     public static final String PRODUCT_TYPE_ALREADY_EXISTS = "Product type already exists";
     public static final String PRODUCT_TYPE_SUCCESS_CREATE = "Success create product type";
     public static final String PRODUCT_TYPE_SUCCESS_FIND = "Success find product type";
@@ -25,10 +25,20 @@ public class MessageConstants {
     public static final String PRODUCT_TYPE_SUCCESS_DELETE = "Success delete product type";
 
     //product
-    public static final String PRODUCT_NOT_FOUND = "Product Not Found";
+    public static final String PRODUCT_NOT_FOUND = "Product not found";
     public static final String PRODUCT_SUCCESS_CREATE = "Success create product";
     public static final String PRODUCT_SUCCESS_FIND = "Success find product";
     public static final String PRODUCT_SUCCESS_FETCH_ALL = "Success fetch all products";
     public static final String PRODUCT_SUCCESS_UPDATE = "Success update product";
     public static final String PRODUCT_SUCCESS_DELETE = "Success delete product";
+
+    //cart
+    public static final String CART_NOT_DEFINED = "Customer cart not defined";
+    public static final String CART_NOT_FOUND = "Cart not found";
+    public static final String CART_ITEM_NOT_FOUND = "Cart item not found";
+    public static final String CART_SUCCESS_ADD_PRODUCT = "Success add product to cart";
+    public static final String CART_SUCCESS_FIND = "Success find cart";
+    public static final String CART_SUCCESS_UPDATE_ITEM = "Success update cart item";
+    public static final String CART_SUCCESS_CLEAR = "Success clear all cart item";
+    public static final String CART_SUCCESS_REMOVE_ITEM = "Success remove cart item";
 }
