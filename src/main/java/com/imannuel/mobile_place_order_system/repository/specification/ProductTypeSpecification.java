b712a1361f23ca1652be5054655728c879a57d44
GtFoBAE05 <y.imannuel@gmail.com>
2025-01-06 00:44:36 +0700

    enchancement: add specificaion method for product and product type

--- src/main/java/com/imannuel/mobile_place_order_system/repository/specification/ProductTypeSpecification.java
+++ src/main/java/com/imannuel/mobile_place_order_system/repository/specification/ProductTypeSpecification.java
@@ -1,12 +1,16 @@
 package com.imannuel.mobile_place_order_system.repository.specification;
 
+import com.imannuel.mobile_place_order_system.entity.Product;
 import com.imannuel.mobile_place_order_system.entity.ProductType;
+import jakarta.persistence.criteria.Join;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.util.StringUtils;
 
 public class ProductTypeSpecification {
-    public static Specification<ProductType> getSpecification(String name){
+    public static Specification<ProductType> getSpecification(String name) {
         Specification<ProductType> specifications = Specification.where(null);
+        specifications = specifications.and(ProductTypeSpecification.hasDeletedFalse());
+
 
         if (StringUtils.hasText(name)) {
             specifications = specifications.and(ProductTypeSpecification.hasName(name));
@@ -18,5 +22,17 @@ public class ProductTypeSpecification {
     private static Specification<ProductType> hasName(String name) {
         return (root, query, criteriaBuilder) ->
                 criteriaBuilder.like(criteriaBuilder.lower(root.get("name")), name.toLowerCase() + "%");
+    }
+
+    private static Specification<ProductType> hasDeletedFalse() {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.isFalse(root.get("deleted"));
+    }
+
+    public static Specification<ProductType> hasProduct(Integer id) {
+        return (root, query, criteriaBuilder) -> {
+            Join<ProductType, Product> products = root.join("productList");
+            return criteriaBuilder.equal(products.get("productType").get("id"), id);
+        };
     }
 }
